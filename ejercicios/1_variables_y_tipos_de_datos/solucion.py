0dc988a9bb899e032f6ab0c95548552298f1835c
Alli3012 <yamilethcse@gmail.com>
2024-12-16 22:00:28 -0500

    Update solucion.py

--- ejercicios/1_variables_y_tipos_de_datos/solucion.py
+++ ejercicios/1_variables_y_tipos_de_datos/solucion.py
@@ -9,3 +9,11 @@ Devuelve un string con el formato:
 """
 
 # Escribe tu solución aquí
+edad = " 25"
+nombre = " Allison"
+print("Mi nombre es" + nombre , "y tengo" + edad , "años")
+
+C:\Users\yamil\PycharmProjects\holamundo\.venv\Scripts\python.exe C:\Users\yamil\PycharmProjects\holamundo\.venv\Scripts\main.py 
+Mi nombre es Allison y tengo 25 años
+
+Process finished with exit code 0
